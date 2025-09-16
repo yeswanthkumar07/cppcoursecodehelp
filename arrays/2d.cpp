cc9c8753d3c73ac585c1932ccb49c004e2d2bd32
lastminutengineer <yeshwantgkv@gmail.com>
2025-09-17 00:48:26 +0530

    2d array

---
+++ arrays/2d.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+   
+
+    // declaration of 2d array :
+    // int arr[3][2];
+
+    // initialisation 
+    int arr[2][4] = {{1,2,3,4},{5,6,7,8}};
+
+    cout << arr[1][3]; // accessing specific element in a array [1][3] are indices (row & coloumn)
+    
+    return 0;
+   
+}
