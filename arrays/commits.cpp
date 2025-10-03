90f1e27bd8660590e6bc20f1545dc85d8a8f59d8
lastminutengineer <yeshwantgkv@gmail.com>
2025-10-03 16:09:16 +0530

    commit

---
+++ arrays/commits.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  // required for cout
+
+using namespace std; // so you don’t need to write std::cout every time
+
+int main() {
+
+    int number ;
+    cout << "enter your marks:" << endl;
+    
+    // taking user input
+
+    cin >> number;
+    if (number == 1)
+    {
+        cout << "good" << endl;
+    }
+    else {
+
+        cout << "not good" << endl;
+    }
+        
+    
+
+}
+
+
